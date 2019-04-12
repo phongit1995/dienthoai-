9d11a636b17e9c063b1068b2bbc4655701ec579a
phongit1995 <40308146+phongit1995@users.noreply.github.com>
2019-04-12 09:16:04 +0700

    add database

--- admin/modules/config.php
+++ admin/modules/config.php
@@ -1,8 +1,8 @@
 <?php 
-	$tenmaychu = "127.0.0.1";
-	$taikhoan = "root";
-	$matkhau = "";
-	$csdl = "bandt";
+	$tenmaychu = "db4free.net:3306";
+	$taikhoan = "phongnguyen123";
+	$matkhau = "phongvip";
+	$csdl = "dienthoai";
 	$conn = mysqli_connect($tenmaychu,$taikhoan,$matkhau,$csdl) or die('khong ket loi duoc!');
 	mysqli_set_charset($conn,'utf8')
  ?>
